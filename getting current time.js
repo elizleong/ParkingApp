d3eb8222856fdfe55308c636e1338acf3b96328c
elizleong <elizsleong@gmail.com>
2015-07-21 15:13:44 -0700

    Create getting current time.js
    gets the current time

---
+++ getting current time.js
@@ -0,0 +1,11 @@
+var d = new Date();
+var curr_hour = d.getHours();
+var curr_min = d.getMinutes();
+
+
+	if (curr_hour + ":" + curr_min == "13:45" ){
+		alert("You can park for free!")
+		}
+	else{
+		alert("You can't park")
+		}
